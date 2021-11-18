dcd50c5deba87df006a60db7e9a07d301cd66e50
Kondla Siva Kumar <92132610+sivareddy9166@users.noreply.github.com>
2021-11-18 12:40:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.54.107.143:9000/',
+  serverUrl: 'http://3.110.122.253:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': 'SivaKondla',
+	    'sonar.password': 'm3ghas!va',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
